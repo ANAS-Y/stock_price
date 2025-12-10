87a63557d84dec318ac8affb1850d123831b953a
ANAS YUNUSA A <54246122+ANAS-Y@users.noreply.github.com>
2025-12-10 17:12:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ model, df_all = load_resources()
 
 # --- 3. UI HEADER ---
 
-st.markdown('<div class="main-header">🇳🇬 Stock Price Forecasting (GRU RNN)</div>', unsafe_allow_html=True)
+st.markdown('<div class="main-header">🇳🇬 Stock Price Forecasting</div>', unsafe_allow_html=True)
 st.markdown('<div class="sub-header">Stock Price Prediction using GRU RNN</div>', unsafe_allow_html=True)
 
 if df_all is None or model is None:
